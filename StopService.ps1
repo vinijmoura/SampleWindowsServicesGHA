1bce7ab6bd9e6963efdd9c2410a1b06b3516c42d
Vinicius Moura <vinijmoura@hotmail.com>
2021-01-13 00:32:55 -0300

    Delete StopService.ps1

--- StopService.ps1
+++
@@ -1,6 +0,0 @@
-param
-(
-  [string] $ServiceName
-)
-  Stop-Service -Name $ServiceName -NoWait
-  do { Write-Host "Stopping service $ServiceName" } until ((Get-Service -Name $ServiceName).Status -eq "Stopped" )
